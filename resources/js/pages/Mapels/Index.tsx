456302ce5e6b2763a14fb9068d4cc89cfde5e840
moshebenzvi <badansiapotaksiap@gmail.com>
2025-04-28 06:07:20 +0700

    add initial Index page and table columns definition

---
+++ resources/js/pages/Mapels/Index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/layouts/app-layout'
+import { Head } from '@inertiajs/react'
+
+export default function Index({/*|*/}) {
+    return (
+        <Layout>
+            <Head title="Index"/>
+
+        </Layout>
+    )
+}
